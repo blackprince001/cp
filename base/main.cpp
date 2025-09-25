0e0aa8a8b61f0fdec89b137718007b67d3a3f486
blackprince001 <appiahboaduprince@gmail.com>
2025-09-25 23:32:05 +0000

    cpp setup for cp and codeforces

---
+++ base/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using vi = vector<int>;
+
+void solve() {
+  int n;
+  cin >> n;
+
+  cout << n << "\n";
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  solve();
+}
